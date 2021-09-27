ae2b0e8232b2d61d77b55d020a7691c9fa2f4be7
syed-fastapp <syed@fastapp.in>
2021-09-27 11:13:33 +0530

    CRUD operations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.sql.functions import mode
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 import schemas
 import models
 import database
@@ -21,7 +20,7 @@ def get_db():
 # def create(title, body):
 #     return {'title':title, 'body':body}
 
-@app.post('/blog')
+@app.post('/blog', status_code=201)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     print(db)
@@ -29,3 +28,36 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).all()
+    return blog
+
+@app.get('/blog/{id}')
+def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} is not available')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with id {id} is not available'}
+    return blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f'Blog with id {id} not available...please give proper id')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'detail': f'Blog with id {id} deleted succesfully'}
+
+@app.put('/blog/{id}', status_code=status.HTTP_200_OK)
+def update_blog(id, request: schemas.Blog,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not available.')
+    blog.update(request)
+    db.commit()
+    return 'updated'
